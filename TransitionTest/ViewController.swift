5e369420b14719246734f85913c91a5d350b377f
Jon Kneller <kneller@gmail.com>
2016-08-03 15:57:21 +0100

    Simplify to actual problem

--- TransitionTest/ViewController.swift
+++ TransitionTest/ViewController.swift
@@ -17,37 +17,20 @@ class ViewController: UITableViewController {
         self.tableView.estimatedRowHeight = 44
     }
 
-    @IBOutlet weak var containerView: UIView!
-    @IBOutlet var bottomConstraint: NSLayoutConstraint!
-    @IBOutlet var bottomConstraint2: NSLayoutConstraint!
-    @IBOutlet weak var view1: UIView!
-    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var flexibleView: View!
     
     @IBAction func togglePressed(_ sender: AnyObject) {
         
         
-        if !self.bottomConstraint.isActive
-        {
-            NSLayoutConstraint.activate([self.bottomConstraint])
-            NSLayoutConstraint.deactivate([self.bottomConstraint2])
-            self.view1.isHidden = false
-            self.view2.isHidden = true
-        }
-        else
-        {
-            NSLayoutConstraint.deactivate([self.bottomConstraint])
-            NSLayoutConstraint.activate([self.bottomConstraint2])
-            self.view1.isHidden = true
-            self.view2.isHidden = false
-        }
+        flexibleView.height = flexibleView.height == CGFloat(200) ? CGFloat(300) : CGFloat(200)
         
-        UIView.transition(with: containerView,
+        UIView.transition(with: flexibleView,
                           duration: 0.3,
                           options: [UIViewAnimationOptions.transitionCrossDissolve, UIViewAnimationOptions.layoutSubviews],
                           animations: {
                             
-                            self.view.setNeedsLayout()
-                            self.view.layoutIfNeeded()
+            self.view.setNeedsLayout()
+            self.view.layoutIfNeeded()
         }, completion: nil)
         
         
@@ -58,27 +41,17 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt heightForRowAtIndexPath: IndexPath) -> CGFloat {
         return UITableViewAutomaticDimension
     }
+}
+
+class View: UIView
+{
+    var height = CGFloat(200) {
+        didSet {
+            invalidateIntrinsicContentSize()
+        }
+    }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = super.tableView(tableView, cellForRowAt: indexPath)
-        cell.layer.borderWidth = 2
-        cell.contentView.layer.borderWidth = 2
-        cell.contentView.layer.borderColor = UIColor.red.cgColor
-        
-        return cell
-    }
-}
-
-class View1: UIView
-{
     override var intrinsicContentSize: CGSize {
-        return CGSize(width: UIViewNoIntrinsicMetric, height: 200)
-    }
-}
-
-class View2: UIView
-{
-    override var intrinsicContentSize: CGSize {
-        return CGSize(width: UIViewNoIntrinsicMetric, height: 100)
+        return CGSize(width: UIViewNoIntrinsicMetric, height: height)
     }
 }
